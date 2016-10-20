3a8b91b06f17eeaf661be216ff2f7ddef2c8d496
lelea2 <daomaikhanh1988@yahoo.com>
2016-10-20 01:48:05 -0700

    Initial check in for android swipte

---
+++ realtor_meet_tinder/app/src/main/java/com/kdao/realtor_meet_tinder/util/Data.java
@@ -0,0 +1,22 @@
+package com.kdao.realtor_meet_tinder.util;
+
+public class Data {
+
+    private String description;
+
+    private String imagePath;
+
+    public Data(String imagePath, String description) {
+        this.imagePath = imagePath;
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+}
